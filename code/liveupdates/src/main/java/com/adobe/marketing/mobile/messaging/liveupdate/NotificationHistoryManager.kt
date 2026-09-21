ca3047b9f9d6d22df8c453c09b5f77629a8539ef
harsheen <harsheenk@adobe.com>
2026-09-21 11:59:03 +0530

    Spotless Apply

--- code/liveupdates/src/main/java/com/adobe/marketing/mobile/messaging/liveupdate/NotificationHistoryManager.kt
+++ code/liveupdates/src/main/java/com/adobe/marketing/mobile/messaging/liveupdate/NotificationHistoryManager.kt
@@ -1,13 +1,13 @@
 /*
-    Copyright 2026 Adobe. All rights reserved.
-    This file is licensed to you under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License. You may obtain a copy
-    of the License at http://www.apache.org/licenses/LICENSE-2.0
-    Unless required by applicable law or agreed to in writing, software distributed under
-    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-    OF ANY KIND, either express or implied. See the License for the specific language
-    governing permissions and limitations under the License.
-  */
+  Copyright 2026 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+*/
 
 package com.adobe.marketing.mobile.messaging.liveupdate
 
@@ -39,7 +39,7 @@ internal object NotificationHistoryManager {
                 LiveUpdatesConstants.LOG_TAG,
                 SELF_TAG,
                 "Dropping Live Update id=${payload.notificationId}: timestamp is older than " +
-                "the 28-day FCM delivery window"
+                    "the 28-day FCM delivery window"
             )
             // TODO: fire an XDM error event for this rejection
             return false
